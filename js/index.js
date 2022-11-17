13718affb84fd766af1b4224c1acaf64e99d0d48
Spacylion <fuerstein@yandex.ru>
2022-11-17 11:23:09 +0300

    merge 1

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-const NFTAddress = "0x5d9a232cc4a572b4fa3717ab048e23c936fddfca";
+const NFTAddress = "0x5d9a232cc4a572b4fa3717ab048e23c936fddfca"
 
 const NFTABI = [
   {
@@ -369,134 +369,134 @@ const NFTABI = [
     stateMutability: "payable",
     type: "function",
   },
-];
+]
 
 let wab3Object,
   web3Modal,
   metaMaskAddress,
   NFTInstance,
   NFTImageURI,
-  tokensOfOwner;
+  tokensOfOwner
 
 window.addEventListener("load", () => {
-  const Web3Modal = window.Web3Modal.default;
+  const Web3Modal = window.Web3Modal.default
   web3Modal = new Web3Modal({
     cacheProvider: false,
     disableInjectedProvider: false,
-  });
+  })
   if (window.ethereum) {
     window.ethereum
       .request({ method: "eth_accounts" })
       .then((accounts) => {
         if (accounts.length) {
           if (window.location.pathname === "/") {
-            document.getElementById("startstory").style.display = "block";
-            document.getElementById("connectwallet").style.display = "none";
+            document.getElementById("startstory").style.display = "block"
+            document.getElementById("connectwallet").style.display = "none"
           }
           if (sessionStorage.getItem("dataUrl")) {
-            handleRendering(sessionStorage.getItem("dataUrl"));
+            handleRendering(sessionStorage.getItem("dataUrl"))
           } else {
-            toHomePage();
+            toHomePage()
           }
         } else {
-          toHomePage();
+          toHomePage()
         }
       })
       .catch((err) => {
-        console.log("err:", err);
-      });
+        console.log("err:", err)
+      })
   } else {
-    toHomePage();
+    toHomePage()
   }
-});
+})
 function toHomePage() {
-  window.location.pathname !== "/" ? (window.location.pathname = "/") : "";
+  window.location.pathname !== "/" ? (window.location.pathname = "/") : ""
 }
 async function onConnect() {
   sessionStorage.clear()
   try {
-    let provider = await web3Modal.connect();
-    onProvider(provider);
+    let provider = await web3Modal.connect()
+    onProvider(provider)
     provider.on("accountsChanged", (accounts) => {
-      console.log("accounts", accounts);
-      onProvider(provider);
-    });
+      console.log("accounts", accounts)
+      onProvider(provider)
+    })
     provider.on("chainChanged", (chainId) => {
-      console.log("chainId", chainId);
-      onProvider(provider);
-    });
+      console.log("chainId", chainId)
+      onProvider(provider)
+    })
   } catch (e) {
-    console.log("Could not get a wallet connection", e);
-    return;
+    console.log("Could not get a wallet connection", e)
+    return
   }
 }
 
 async function onProvider(provider) {
   try {
-    wab3Object = new Web3(provider);
-    let accounts = await wab3Object.eth.getAccounts();
-    metaMaskAddress = accounts[0];
-    console.log(metaMaskAddress);
-    NFTInstance = new wab3Object.eth.Contract(NFTABI, NFTAddress);
-    document.getElementById("startstory").style.display = "block";
-    document.getElementById("connectwallet").style.display = "none";
-    readValue();
+    wab3Object = new Web3(provider)
+    let accounts = await wab3Object.eth.getAccounts()
+    metaMaskAddress = accounts[0]
+    console.log(metaMaskAddress)
+    NFTInstance = new wab3Object.eth.Contract(NFTABI, NFTAddress)
+    document.getElementById("startstory").style.display = "block"
+    document.getElementById("connectwallet").style.display = "none"
+    readValue()
   } catch (e) {
-    console.log("Could not get a wallet connection", e);
-    return;
+    console.log("Could not get a wallet connection", e)
+    return
   }
 }
 
 function readValue() {
   Promise.all([NFTInstance.methods.tokensOfOwner(metaMaskAddress).call()]).then(
     async ([tokensOfOwner]) => {
-      console.log("tokensOfOwner:", tokensOfOwner);
-      tokensOfOwner = tokensOfOwner;
+      console.log("tokensOfOwner:", tokensOfOwner)
+      tokensOfOwner = tokensOfOwner
       if (tokensOfOwner.length) {
         let tokenURI = await NFTInstance.methods
           .tokenURI(tokensOfOwner[0])
-          .call();
-        console.log("tokenURI:", tokenURI);
-        tokenURI = tokenURI.replaceAll("://", "/");
-        let tokenURL = "https://sartoshigob.mypinata.cloud/" + tokenURI;
-        console.log("tokenURL:", tokenURL);
-        fetchJSON(tokenURL);
+          .call()
+        console.log("tokenURI:", tokenURI)
+        tokenURI = tokenURI.replaceAll("://", "/")
+        let tokenURL = "https://sartoshigob.mypinata.cloud/" + tokenURI
+        console.log("tokenURL:", tokenURL)
+        fetchJSON(tokenURL)
       } else {
-        alert("No NFT found");
+        alert("No NFT found")
       }
     }
-  );
+  )
 }
 function fetchJSON(URL) {
   fetch(URL)
     .then(async (response) => {
-      let res = await response.json();
-      console.log("response:", res);
-      NFTImageURI = res.image;
-      console.log("NFTImageURI:", NFTImageURI);
-      NFTImageURI = NFTImageURI.replaceAll("://", "/");
-      let NFTImageURL = "https://sartoshigob.mypinata.cloud/" + NFTImageURI;
-      console.log("NFTImageURL:", NFTImageURL);
+      let res = await response.json()
+      console.log("response:", res)
+      NFTImageURI = res.image
+      console.log("NFTImageURI:", NFTImageURI)
+      NFTImageURI = NFTImageURI.replaceAll("://", "/")
+      let NFTImageURL = "https://sartoshigob.mypinata.cloud/" + NFTImageURI
+      console.log("NFTImageURL:", NFTImageURL)
       imagetoBase64(NFTImageURL, (dataUrl) => {
-        sessionStorage.setItem("dataUrl", dataUrl);
-      });
+        sessionStorage.setItem("dataUrl", dataUrl)
+      })
     })
     .catch((error) => {
-      console.log("error:", error);
-    });
+      console.log("error:", error)
+    })
 }
 function humanized(number, fix) {
-  return Number(number.toString() / 1e9).toFixed(number == 0 ? 3 : fix);
+  return Number(number.toString() / 1e9).toFixed(number == 0 ? 3 : fix)
 }
 
 function addrTruncation(addr) {
-  return addr.slice(0, 6) + "…" + addr.slice(addr.length - 4, addr.length);
+  return addr.slice(0, 6) + "…" + addr.slice(addr.length - 4, addr.length)
 }
 
 function disconnect() {
-  wab3Object = null;
-  NFTInstance = null;
-  metaMaskAddress = null;
+  wab3Object = null
+  NFTInstance = null
+  metaMaskAddress = null
 }
 
 function notify(msg) {
@@ -505,69 +505,69 @@ function notify(msg) {
     duration: 3000,
     gravity: "bottom",
     position: "right",
-  }).showToast();
+  }).showToast()
 }
 
 function imagetoBase64(url, callback) {
-  let xhRequest = new XMLHttpRequest();
+  let xhRequest = new XMLHttpRequest()
   xhRequest.onload = function () {
-    let reader = new FileReader();
+    let reader = new FileReader()
     reader.onloadend = function () {
-      callback(reader.result);
-    };
-    reader.readAsDataURL(xhRequest.response);
-  };
-  xhRequest.open("GET", url);
-  xhRequest.responseType = "blob";
-  xhRequest.send();
+      callback(reader.result)
+    }
+    reader.readAsDataURL(xhRequest.response)
+  }
+  xhRequest.open("GET", url)
+  xhRequest.responseType = "blob"
+  xhRequest.send()
 }
 
 function handleRendering(dataUrl) {
-  let ele;
+  let ele
   switch (window.location.pathname) {
     case "/page-1.html":
-      ele = document.getElementsByClassName("page2NFT")[0];
-      ele.src = dataUrl;
-      break;
+      ele = document.getElementsByClassName("page2NFT")[0]
+      ele.src = dataUrl
+      break
     case "/page-2.html":
-      ele = document.getElementsByClassName("bg-shelf-img-pg2")[0];
-      ele.src = dataUrl;
-      ele.srcset = dataUrl;
-      ele.style.rotate = "90deg";
-      break;
+      ele = document.getElementsByClassName("bg-shelf-img-pg2")[0]
+      ele.src = dataUrl
+      ele.srcset = dataUrl
+      ele.style.rotate = "90deg"
+      break
     case "/page-3.html":
-      ele = document.getElementsByClassName("page-3-nft")[0];
-      ele.src = dataUrl;
-      break;
+      ele = document.getElementsByClassName("page-3-nft")[0]
+      ele.src = dataUrl
+      break
     case "/page-4.html":
-      ele = document.getElementsByClassName("page-4-nft")[0];
-      ele.src = dataUrl;
-      ele.srcset = dataUrl;
-      break;
+      ele = document.getElementsByClassName("page-4-nft")[0]
+      ele.src = dataUrl
+      ele.srcset = dataUrl
+      break
     case "/page-5.html":
-      ele = document.getElementsByClassName("page-5-nft")[0];
-      ele.src = dataUrl;
-      ele.srcset = dataUrl;
-      break;
+      ele = document.getElementsByClassName("page-5-nft")[0]
+      ele.src = dataUrl
+      ele.srcset = dataUrl
+      break
     case "/page-6.html":
-      ele = document.getElementsByClassName("page-6-nft")[0];
-      ele.src = dataUrl;
-      ele.srcset = dataUrl;
-      break;
+      ele = document.getElementsByClassName("page-6-nft")[0]
+      ele.src = dataUrl
+      ele.srcset = dataUrl
+      break
     case "/page-7.html":
-      ele = document.getElementsByClassName("page-7-nft")[0];
-      ele.src = dataUrl;
-      ele.srcset = dataUrl;
-      break;
+      ele = document.getElementsByClassName("page-7-nft")[0]
+      ele.src = dataUrl
+      ele.srcset = dataUrl
+      break
     case "/page-8.html":
-      ele = document.getElementsByClassName("page-8-nft")[0];
-      ele.src = dataUrl;
-      ele.srcset = dataUrl;
-      break;
+      ele = document.getElementsByClassName("page-8-nft")[0]
+      ele.src = dataUrl
+      ele.srcset = dataUrl
+      break
     case "/page-9.html":
-      ele = document.getElementsByClassName("page-9-nft")[0];
-      ele.src = dataUrl;
-      ele.srcset = dataUrl;
-      break;
+      ele = document.getElementsByClassName("page-9-nft")[0]
+      ele.src = dataUrl
+      ele.srcset = dataUrl
+      break
   }
 }
